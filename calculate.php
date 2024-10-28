3ac1236ab36c510295b65a29f505fa96e086f036
Zainalithegreat <zain.ali@pcc.edu>
2024-10-28 03:01:59 -0700

    Initial commit

---
+++ calculate.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 'hello world';
+
+$diameter = $_GET['diameter'];
+$depth = $_GET['depth'];
+$radius = $diameter / 2;
+$volume = pi() * $radius ** 2 * $depth;
+$gallons = $volume * 7.48052;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        A Swimming Pool Calculator!
+    </title>
+</head>
+<body>
+    <h1>Swimming Pool Calculator</h1>
+    <p>The volume of a swimming pool that is <?php echo $diameter; ?> in diameter and <?php echo $depth; ?> deep in <?php echo number_format($gallons, 1)?> gallons</p>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        A Swimming Pool Calculator!
+    </title>
+</head>
+<body>
+<h1>Swimming Pool Calculator</h1>
+<p>How mych water will it take to fill an above-ground pool?</p>
+    <form method="get" action="calculate.php">
+        <table>
+            <tr><td>Enter the diameter:</td><td><input type="text" name="diameter" width="100"</td></tr>
+            <tr><td>Enter the depth:</td><td><input type="text" name="depth" width="100"</td></tr>
+            <tr><td><input type="submit" value="Submit"</td></tr>
+        </table>
+    </form>
+</body>
+</html>
